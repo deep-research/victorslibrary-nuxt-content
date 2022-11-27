8baeb120603eacbbef9d6f9116216a8ce6396243
Victor <fisheve@mx.lakeforest.edu>
2022-11-27 04:00:13 -0600

    Trying both not-found and empty slot

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -8,6 +8,12 @@
             <NuxtLink to="/">Home Page</NuxtLink>
           </p>
         </template>
+        <template #empty>
+          <h1>Document is empty</h1>
+          <p>
+            <NuxtLink to="/">Home Page</NuxtLink>
+          </p>
+        </template>
       </ContentDoc>
     </main>
   </NuxtLayout>
